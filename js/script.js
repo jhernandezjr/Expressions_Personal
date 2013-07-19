35dfb8478a31ee9df79b59793b91000ed36a374a
Jose.Hernandez <joey.hernandezjr@gmail.com>
2013-07-18 20:01:16 -0400

    first commit

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
-alert("This is in my js file");
+var length = prompt("To determine which size box you will need for your printer please enter the length")
+var width = prompt("Enter width of your printer:");("Please enter the width")
+var height = prompt("Enter height of your printer:");("Please ent the height")
+var dimensions = length * width * height;
+
+var result = "The dimensions of the printer is " + dimensions + " inches";
+alert(result);
+
+//console.log(dimensions);
